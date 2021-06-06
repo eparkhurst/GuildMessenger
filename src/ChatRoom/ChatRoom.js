8225dfb3af287ca86eb4719f9affed98c92b2348
eparkhurst <eli.parkhurst@gmail.com>
2021-06-06 14:37:10 -0600

    can add messages. It's basic but meets mvp criteria

--- src/ChatRoom/ChatRoom.js
+++ src/ChatRoom/ChatRoom.js
@@ -1,14 +1,32 @@
 import 'firebase/firestore';
-
+import { useState } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 function ChatRoom({firestore}){
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
   const [messages] = useCollectionData(query, {idField: 'id'})
+
+  const [newMessage, updateMessage] = useState('')
+
+  const onSend = async () => {
+    await messagesRef.add({
+      text: newMessage,
+      createdAt: Date.now(),
+    })
+    updateMessage('')
+    
+  }
   return (
     <div>
         {messages && messages.map(msg => <p>{msg.text}</p>)}
+        <div>
+          <input value={newMessage} onChange={(e)=> {
+            updateMessage(e.target.value);
+            
+          }}></input>
+          <button onClick={onSend}>Send</button>
+        </div>
     </div>
   )
 }
