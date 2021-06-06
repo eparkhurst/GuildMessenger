7f625bb77c3d90ac17fa33e6e5e92703839b20a4
eparkhurst <eli.parkhurst@gmail.com>
2021-06-06 13:28:20 -0600

    initial commit. Created react app and pulled in firestore but it is not used yet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCFnE2WurJNqROEf7HqK5e-NfCu7kdMpqQ",
+    authDomain: "guildmessenger-354ac.firebaseapp.com",
+    projectId: "guildmessenger-354ac",
+    storageBucket: "guildmessenger-354ac.appspot.com",
+    messagingSenderId: "892289199455",
+    appId: "1:892289199455:web:5a4df0a8a9ed98310e9aea",
+    measurementId: "G-22VWKPZESC"
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Guild Messenger
       </header>
     </div>
   );
