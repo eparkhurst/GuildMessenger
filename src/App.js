3c23ac3938c7e1719e3e2b94fd6fd85b0cff7a96
eparkhurst <eli.parkhurst@gmail.com>
2021-06-06 14:18:54 -0600

    have firestore working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import './App.css';
+// import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth';
 
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
   apiKey: "AIzaSyCFnE2WurJNqROEf7HqK5e-NfCu7kdMpqQ",
-    authDomain: "guildmessenger-354ac.firebaseapp.com",
-    projectId: "guildmessenger-354ac",
-    storageBucket: "guildmessenger-354ac.appspot.com",
-    messagingSenderId: "892289199455",
-    appId: "1:892289199455:web:5a4df0a8a9ed98310e9aea",
-    measurementId: "G-22VWKPZESC"
-})
+  authDomain: "guildmessenger-354ac.firebaseapp.com",
+  projectId: "guildmessenger-354ac",
+  storageBucket: "guildmessenger-354ac.appspot.com",
+  messagingSenderId: "892289199455",
+  appId: "1:892289199455:web:5a4df0a8a9ed98310e9aea",
+  measurementId: "G-22VWKPZESC"
+});
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Guild Messenger
-      </header>
+      <header className="App-header">Guild Messenger</header>
+      <section>
+        <ChatRoom /> 
+      </section>
     </div>
   );
+}
+
+function ChatRoom(){
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, {idField: 'id'})
+  return (
+    <div>
+        {messages && messages.map(msg => <p>{msg.text}</p>)}
+    </div>
+  )
 }
 
 export default App;
